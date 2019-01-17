77cb1424ef0d1eda6c8bfbd6de1773cbbdbd79de
Your Name <you@example.com>
2019-01-17 08:50:24 +0800

    fisirt init

---
+++ main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main(){
+
+    printf("jinruminganzifguxitomng");
+    while(1)
+    {
+        printf("1---TIAN JIA\n");
+        printf( "2---shan chu\n");
+        printf("3--cha kan\n");
+        printf("4---ti huuan\n");
+        printf("5---tui chu\n");
+        printf("qing shu ru xfiang yingde xu hao\n");
+        int code;
+          scanf("%d",&code);
+          if(code==1)
+          {
+          //printf("qingtianjiaminganzifu\n");
+          //char m;
+          //scanf("%c",&m);
+          //scanf("%c";&m);
+          }
+          if(code==2)
+          {
+
+          }
+          if(code==3)
+          {
+
+          }
+          if(code==4)
+          {
+
+          }
+          if(code==5)
+          {
+
+          }
+    }
+
+    return 0;
+
+}
